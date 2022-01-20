e44b3b0dbea9ec2f4245a6c23df452fbfa559ba5
Karltun Moreno (javacode.crypto) <95730440+karltunmoreno@users.noreply.github.com>
2022-01-20 16:34:17 -0500

    Second Push Pending

---
+++ assets/js/script.js
@@ -0,0 +1,119 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById ('next-btn')
+('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+
+let shuffledQuestions, currentQuestionIndex
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+currentQuestionIndex++
+setNextQuestion()
+})
+
+
+function startGame(){
+startButton.classList.add('hide')
+shuffledQuestions = questions.sort(() => Math.random() - .5)
+currentQuestionIndex = 0 
+questionContainerElement.classList.remove('hide')
+setNextQuestion()
+}
+
+function setNextQuestion(){
+    resetState()
+showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question){
+ questionElement.innerText = question.question
+ question.answers.forEach(answer => {
+     const button = document.createElement('button')
+     button.innerText = answer.text
+     button.classList.add('btn')
+     if(answer.correct){
+         button.dataset.correct = answer.correct
+     }
+     button.addEventListener('click', selectAnswer)
+     answerButtonsElement.appendChild(button)
+ })
+}
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElements.firstChild)
+    }
+}
+function selectAnswer(){
+const selectedButton = e.target
+const correct = selectedButton.dataset.correct
+setStatusClass(document.body, correct)
+Array.from(answerButtonsElement.children).forEach(button =>{
+setStatusClass(button, button.dataset.correct)
+})
+if (shuffledQuestions.length > currentQuestionIndex + 1) {
+nextButton.classList.remove('hide')
+} else {
+startButton.innerText = 'Restart'
+startButton.classList.remove('hide')
+  }
+}
+
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+    element.classlist.add('correct')
+    } else {
+        element.classlist.add('wrong')  
+    }
+}
+
+function clearStatusClass(element){
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+
+}
+
+const question = [
+{
+    question: 'What is 2+2?',
+    answers: [
+        {text: '4', correct: true },
+        {text: '22', correct: false }
+    ]
+    },
+    {
+        question: 'what is a styling code language?',
+        answers: [
+            {text: 'JAVA', correct: false },
+            {text: 'CSS', correct: true },
+            {text: 'HTML', correct: false },
+            {text: 'LOOPS', correct: false }
+    ]          
+},
+{
+    question: 'What do we use to link CSS to HTML in the <Head>?',
+    answers: [
+        {text: 'loops', correct: false },
+        {text: 'arrays', correct: false },
+        {text: 'href=./assets/css/styles.css', correct: true },
+        {text: 'variables', correct: false }
+
+]
+},
+{
+    question: 'What is Javascipt?',
+    answers: [
+        {text: 'a dummy', correct: false },
+        {text: 'programming Launguage', correct: true },
+        {text: 'data recovery', correct: false },
+        {text: 'css', correct: false }
+    
+{
+    buttonlink.addEventListener ('click', startgame)
+
+    
